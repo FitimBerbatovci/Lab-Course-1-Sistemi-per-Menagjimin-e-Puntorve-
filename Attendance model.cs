1b7100029023f07b777d7eebd25c30c61e393c7b
FitimBerbatovci <121439680+FitimBerbatovci@users.noreply.github.com>
2024-04-21 14:27:23 +0200

    Attendance model .net core

---
+++ Attendance model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YourNamespace.Models
+{
+    public class Attendance
+    {
+        public int Id { get; set; }
+        public string EmployeeName { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsPresent { get; set; }
+    }
+}
+

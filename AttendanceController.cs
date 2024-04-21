e18df450705dd0490c460cdb82859ec999404283
FitimBerbatovci <121439680+FitimBerbatovci@users.noreply.github.com>
2024-04-21 14:26:50 +0200

    AttendanceController .net core

---
+++ AttendanceController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YourNamespace.Models;
+
+namespace YourNamespace.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttendanceController : ControllerBase
+    {
+        private readonly YourDbContext _context;
+
+        public AttendanceController(YourDbContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/Attendance
+        [HttpPost]
+        public async Task<ActionResult<Attendance>> PostAttendance(Attendance attendance)
+        {
+            _context.Attendance.Add(attendance);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAttendance), new { id = attendance.Id }, attendance);
+        }
+
+        // GET: api/Attendance/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Attendance>> GetAttendance(int id)
+        {
+            var attendance = await _context.Attendance.FindAsync(id);
+
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+
+            return attendance;
+        }
+    }
+}
